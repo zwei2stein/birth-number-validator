11f53b4a7890bb8b28f641005f8e6824833d2b7f
zwei2stein <zwei2stein@gmail.com>
2013-02-13 10:34:51 +0000

    Initial commit

---
+++ src/cz/zweistein/czechvalidations/Sex.java
@@ -0,0 +1,16 @@
+package cz.zweistein.czechvalidations;
+
+/**
+ * 
+ * Simple sex enum used as one of possible inputs to BirthNumberValidator
+ * 
+ * @author Petr Prokop
+ *
+ */
+public enum Sex {
+	
+	MALE,
+	FEMALE,
+	UNSPECIFICED;
+
+}
